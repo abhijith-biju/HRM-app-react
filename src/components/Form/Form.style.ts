e042a18a9b24eda62ad918fc43a58a58c4c08d86
Abhijith Biju <abhijith.biju@qburst.com>
2023-11-06 17:33:55 +0530

    fix in routing

--- src/components/Form/Form.style.ts
+++ src/components/Form/Form.style.ts
@@ -1,9 +1,9 @@
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-const StyledForm = styled.form`
-    & .form-controls-container {
-        justify-content: end;
-        gap: 20px;
-        margin: 10px 0 0 0;
-    }
-`;
+// const StyledForm = styled.form`
+//     & .form-controls-container {
+//         justify-content: end;
+//         gap: 20px;
+//         margin: 10px 0 0 0;
+//     }
+// `;
