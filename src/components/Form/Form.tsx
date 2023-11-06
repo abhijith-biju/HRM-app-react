7b49ed600344b8a570359461bcda305b1c20d39a
Abhijith Biju <abhijith.biju@qburst.com>
2023-11-06 17:36:20 +0530

    fix in routing

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,20 +1,20 @@
-import { Flex } from '..';
-import { Button } from '..';
+// import { Flex } from '..';
+// import { Button } from '..';
 
-interface IForm {
-    children: React.ReactNode;
-}
+// interface IForm {
+//     children: React.ReactNode;
+// }
 
-const Form: React.FC<IForm> = ({ children }) => {
-    return (
-        <form>
-            {children}
-            <Flex>
-                <div className="flex-container form-controls-container">
-                    <Button className="primary">SUBMIT</Button>
-                    <Button className="outline">CANCEL</Button>
-                </div>
-            </Flex>
-        </form>
-    );
-};
+// const Form: React.FC<IForm> = ({ children }) => {
+//     return (
+//         <form>
+//             {children}
+//             <Flex>
+//                 <div className="flex-container form-controls-container">
+//                     <Button className="primary">SUBMIT</Button>
+//                     <Button className="outline">CANCEL</Button>
+//                 </div>
+//             </Flex>
+//         </form>
+//     );
+// };
